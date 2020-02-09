07d5aea9221e9e7dc7ef1fd2464b5221d62f03ca
Nick Wing <git@mythgaming.net>
2020-02-09 17:20:48 +0000

    add dataloader to userservice

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,11 @@
 export function toArray<ItemType> (items: ItemType | ItemType[]) {
   return Array.isArray(items) ? items : [items]
 }
+
+export function toClass<ItemType> (items: any[], ObjectClass: { new(): ItemType }) {
+  return items.map(item => {
+    const ret = new ObjectClass()
+    Object.assign(ret, item)
+    return ret
+  })
+}
--- src/services/userservice.ts
+++ src/services/userservice.ts
@@ -1,9 +1,18 @@
-import { MongoID } from '../lib'
+import { MongoID, toClass } from '../lib'
 import { BaseService } from '.'
 import { db } from '../lib/db'
+import { DataLoaderFactory } from 'dataloader-factory'
+import { User } from '../models'
+
+DataLoaderFactory.register<MongoID, User>('users', {
+  fetch: async (ids: MongoID[]) => {
+    const users = await db.collection('users').find({ id: ids }).toArray()
+    return toClass(users, User)
+  }
+})
 
 export class UserService extends BaseService {
   async get (id: MongoID) {
-    return db.collection('users').findOne({ id })
+    return this.dataLoaderFactory.get<MongoID, User>('users').load(id)
   }
 }
