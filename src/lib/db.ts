68d434a7e7502849c125352a7c0975856e2172e3
Nick Wing <git@mythgaming.net>
2020-03-02 00:29:40 +0000

    working on prod docker-compose for later, unfinished

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,7 +5,9 @@ class Mongo {
   public db!: Db
 
   async start () {
-    this.client = await MongoClient.connect(`mongodb://${process.env.DB_HOST || 'adventure-notes-db'}:${process.env.DB_PORT || 27017}`, {
+    if (process.env.DB_PASS === 'overridethis') throw new Error('Using the default password for production is not allowed. Create a docker-compose.prod.override.yml file and provide a secure password.')
+    const userpass = process.env.DB_USER ? `${process.env.DB_USER}:${process.env.DB_PASS}@` : ''
+    this.client = await MongoClient.connect(`mongodb://${userpass}${process.env.DB_HOST || 'adventure-notes-db'}:${process.env.DB_PORT || 27017}`, {
       useUnifiedTopology: true
     })
     this.db = this.client.db(process.env.DB_DATABASE || 'adventurenotes')
